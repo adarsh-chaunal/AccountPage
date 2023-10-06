da0d980bcfd782602d95910def696301d60522cc
adarsh-chaunal <adarsh97@gmail.com>
2023-10-06 09:19:45 +0530

    Add project files.

---
+++ Account.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Account.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Account.DataAccess.Data;
+
+public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);//Keys of identity tables are mapped to on model creating. If this method is not called, then you will run into the error message
+    }
+
+}
+
